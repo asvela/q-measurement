b56988a72848a09f8e489f9b1e623eae9e8b57d4
Andreas Svela <svela.andreas@gmail.com>
2019-09-02 16:06:56 +0100

    Initial commit

---
+++ qfactor.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+Andreas Svela // 2019
+"""
+
+import sys, logging
+import logging; _log = logging.getLogger(__name__)
+import pandas as pd             #dataframes
+import numpy as np              #arrays
+import matplotlib.pyplot as plt #plotting
+import scipy.signal as sig
+import scipy.optimize as opt    #fitting
+from matplotlib import gridspec
+from datetime import datetime
+
+_filetype = ".csv"
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+def read_data(fname, ext=_filetype, usecols=[0, 1], names=['time', 'ch1']):
+    df = pd.read_csv(fname+ext, skiprows=[0, 1, 2], usecols=usecols, names=names)
+    return [df[col].values for col in names]
+
+def frequency_span_per_sec(scan_freq, peak_to_peak, scaling, calibration):
+    """Calculate frequency span per second for the laser in MHz per second:
+    scan_freq in Hz,  peak_to_peak in volts,  scaling in mA/volt,
+    calibration in MHz/mA"""
+    scan_period = 1/(2*scan_freq) #sec, division by two because of triangle wave and hence in practice sweeping double the speed
+    current_span = peak_to_peak*scaling #mA
+    return current_span*calibration/scan_period #MHz/second
+
+def scale_channel(channel, actual_zero=0, new_min=0, new_max=1, max_func=np.max):
+    """Scale the channels, mapping them from actual zeros and max value to 0 to 1"""
+    zero = actual_zero if actual_zero is not None else np.min(channel)
+    channel = channel - zero
+    corrected_max = max_func(channel)
+    old_range = corrected_max
+    new_range = new_max - new_min
+    scaling = new_range/old_range
+    return channel*scaling + new_min
+
+moving_average = lambda oneD_array, window: np.convolve(oneD_array, np.ones((window,))/window, mode='valid')
+
+def lor_fit(f, background, amp, f0, linewidth):
+    return background - amp/(1 + (f - f0)**2/(linewidth/2)**2)
+
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+##                                   MAIN                                     ##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+if __name__ == '__main__':
+
+    # folder = "T:/DATA/Microcombs/Experiments/Near-field/Code/Q-measurement/"
+    folder = "T:/DATA/Microcombs/Experiments/Near-field/Code/nearfield-control/"
+    fname = "20190830(6) 1550+75V"
+    wavelength = 1550e-9 #m
+    freq_per_sec = frequency_span_per_sec(scan_freq=1007, peak_to_peak=2, scaling=10, calibration=8.2)
+    pump_freq = 3e8/wavelength/1e6 #MHz
+
+    # read data
+    time, ch1, ch2 = read_data(folder+fname, usecols=[0,1,2], names=['time', 'ch1', 'ch2'])
+    ch1 = scale_channel(ch1, max_func=np.mean)
+    freq = time*freq_per_sec
+
+    # fit the trace
+    ## guess on the form (background, amp, f0, linewidth)
+    guess = (np.mean(ch1), np.max(ch1)-np.min(ch1), freq[np.argmin(ch1)], 0.5)
+    bounds = ((0, 0, -np.inf, 0), (np.inf, np.inf, np.inf, np.inf))
+    ## truncate the trace
+    indices = ((guess[2]-10*guess[3]) < freq) & (freq < (guess[2]+10*guess[3]))
+    trunc_freq = freq[indices]
+    trunc_ch1 = ch1[indices]
+    ## run fit and extract parameters
+    optp, _ = opt.curve_fit(lor_fit, trunc_freq, trunc_ch1, p0=guess, bounds=bounds)
+    background, amp, f0, linewidth = optp
+    fitline = lor_fit(trunc_freq, *optp)
+
+    # calculate the Q
+    Q = pump_freq/linewidth
+    Q_str = "Q = %.1fe8" % (Q/1e8)
+    print(Q_str)
+
+    # plot the fit and linewidth
+    ax = plt.subplot()
+    ax.plot(freq-f0, ch1)
+    # ax.plot(freq, lor_fit(freq, *guess))
+    ax.plot(trunc_freq-f0, fitline)
+    ax.set_xlim([-8*linewidth, 8*linewidth])
+    ax.set_xlabel("Frequency offset [MHz]")
+    ax.set_ylabel("Transmission")
+    ax.set_title(fname+": "+Q_str)
+    ax.hlines(y=amp/2+np.min(fitline), xmin=-linewidth/2, xmax=linewidth/2)
+    ax.annotate(Q_str, (-linewidth, background), xycoords='data')
+    plt.tight_layout()
+    plt.savefig(folder+fname+(" Q%.0fe8"%(Q/1e8)))
+    plt.show()
---
+++ trace_truncation.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+Andreas Svela // 2019
+"""
+
+import pandas as pd             #dataframes
+import numpy as np              #arrays
+import matplotlib.pyplot as plt #plotting
+import scipy.signal as sig
+from matplotlib import gridspec
+from datetime import datetime
+
+_filetype = ".csv"
+
+
+## Load data and calculate spectra ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+def read_data(fname, ext=_filetype, usecols=[0, 1], names=['time', 'ch1']):
+    df = pd.read_csv(fname+ext, skiprows=[0, 1, 2], usecols=usecols, names=names)
+    return [df[col].values for col in names]
+
+## Interactive timeseries truncation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+class SetTimeseriesWindowing():
+    """Interactive plot:
+      * Shows original timeseries, left (right) click sets lower (upper)
+        cut-off for the truncated timeseries.
+      * Fine adjustment possible in separate smaller plots for lower and
+        upper cut-off (period_margin sets the number of periods displayed
+        on either side of the cut-off).
+      * Double click outside axes saves the truncated time trace to file."""
+
+    def __init__(self, pathfname, time, channels, labels, relative_margin=0.1):
+        # initialise attributes:
+        self.pathfname, self.margin = pathfname, relative_margin
+        self.time, self.chs, self.labels = time, channels, labels
+        self.lower_ix, self.upper_ix = 0, -1
+        # calculate scaled power spetral densities
+        self.truncate()
+        # initialise the figure and connect the button_press_event
+        self.fig, self.ax = self.initialise_plot()
+        self.canvas = self.fig.canvas
+        self.cid = self.canvas.mpl_connect('button_press_event', self.onclick)
+        # update to plot the data and show the figure
+        self.update_plot()
+        plt.show()
+
+    def truncate(self):
+        """Calculate the truncated timeseries and spectra"""
+        self.trunc_time = self.time[self.lower_ix:self.upper_ix]
+        self.trunc_chs = [ch[self.lower_ix:self.upper_ix] for ch in self.chs]
+
+    def initialise_plot(self, figsize=(8, 9)):
+        """Create figure and axis objects, plot data and return objects"""
+        fig = plt.figure(figsize=figsize)
+        # creating subplots of different size:
+        gs = gridspec.GridSpec(3, 2)
+        ax = [plt.subplot(gs[0, :]),
+              plt.subplot(gs[2, :]),
+              plt.subplot(gs[1, 0]),
+              plt.subplot(gs[1, 1])]
+        self.add_labels(ax)
+        plt.tight_layout()
+        return fig, ax
+
+    def onclick(self, event):
+        """Handling a click event: save truncated series if double click,
+        otherwise set new limits"""
+        if event.dblclick: # save the current truncated version
+            print("\nSave the current truncated time series trace with to")
+            print("%s_[postfix]%s" % (self.pathfname, _filetype))
+            postfix = input("Specify [postfix], or n+enter to abort: ")
+            if postfix.lower() == 'n':
+                print("Not saved")
+            else:
+                date_time = str(datetime.now()) # get current date and time
+                head = "time,"+','.join(self.labels)+"\n"
+                # shape data in correct from
+                x = np.array([self.trunc_time]).T
+                y = np.array([ch for ch in self.trunc_chs]).T
+                data = np.append(x, y, axis=1)
+                np.savetxt(self.pathfname+"_"+postfix+_filetype, data, delimiter=",", header=head+date_time)
+                plt.savefig(self.pathfname+"_"+postfix)
+                print("Saved")
+        elif event.inaxes in self.ax: # click in any of the axes
+            ix, _ = find_nearest(self.time, event.xdata)
+            if event.inaxes == self.ax[0]: # click in original timeseries
+                if event.button == 1: # if left click
+                    self.set_ix(new_l=ix)
+                elif event.button == 3: # right click
+                    self.set_ix(new_u=ix)
+            elif event.inaxes == self.ax[1]: # click in lower lim plot
+                self.set_ix(new_l=ix)
+            elif event.inaxes == self.ax[3]: # click in upper lim plot
+                self.set_ix(new_u=ix)
+            # redraw the plots with updated indecies
+            self.update_plot()
+
+    def set_ix(self, new_l=None, new_u=None):
+        """Set new lower/upper indices if new indecies span a range"""
+        if new_l is not None and (new_l < self.upper_ix or self.upper_ix == -1): self.lower_ix = new_l
+        if new_u is not None and new_u > self.lower_ix: self.upper_ix = new_u
+
+    def update_plot(self, alpha=0.8):
+        """Redraw the axes with
+            * lines indicating the points selected so far
+            * updated truncated timeseries
+            * updated spectra"""
+        # calculate truncated spectra
+        self.truncate()
+        for a in self.ax: a.clear()
+        # plot the time series and spectra
+        for ch, label in zip(self.chs, self.labels):
+            self.ax[0].plot(self.time, ch, label=label, alpha=alpha)
+            self.ax[2].plot(self.time, ch-np.mean(ch), label=label, alpha=alpha)
+            self.ax[3].plot(self.time, ch-np.mean(ch), label=label, alpha=alpha)
+        # plot truncated time series
+        for ch in self.trunc_chs:
+            self.ax[1].plot(self.trunc_time, ch, alpha=alpha)
+        # plot axislines for the cut offs and enable gridlines on spectrum
+        for a in [self.ax[0], self.ax[2], self.ax[3]]:
+            a.axvline(self.time[self.lower_ix], c='r')
+            a.axvline(self.time[self.upper_ix], c='b')
+        # set limits for smaller plots:
+        time_margin = self.margin*(time[-1]-time[0])
+        for a, ix in zip([self.ax[2], self.ax[3]], [self.lower_ix, self.upper_ix]):
+            a.set_xlim(self.time[ix]-time_margin, self.time[ix]+time_margin)
+        # add labels and draw the canvas
+        self.add_labels()
+        self.canvas.draw()
+
+    def add_labels(self, ax=None, freq_lims=[0, 200], spectrum_lims=[1e-12, 1e-5]):
+        """Set the axis labels and limits of axes in the vector ax, or
+        when None uses self.ax"""
+        ax = ax if ax is not None else self.ax
+        ax[0].legend()
+        for a in ax[0:2]:
+            a.set_xlabel("Time [s]")
+            a.set_ylabel("Transmission signal")
+        ax[0].set_title("Original time series")
+        ax[0].set_xlim([min(self.time), max(self.time)])
+        ax[1].set_title("Truncated time series")
+        ax[1].set_xlim([min(self.trunc_time), max(self.trunc_time)])
+        ax[2].set_title("Lower cut-off")
+        ax[3].set_title("Upper cut-off")
+
+def find_nearest(array, value):
+    """Return index and value closest to 'value' in 'array'"""
+    array = np.asarray(array)
+    ix = (np.abs(array - value)).argmin()
+    return ix, array[ix]
+
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+##                                   MAIN                                     ##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
+folder = "T:/DATA/Microcombs/Experiments/Near-field/Code/data/Laser sweep calibration/Internal voltage sweep/"
+fnames = ["15"+str(i)+" AVER4" for i in range(53,56)]
+
+if __name__ == '__main__':
+    for fname in fnames:
+        time, ch1, ch2 = read_data(folder+fname, usecols=[0,1,2], names=['time', 'ch1', 'ch2'])
+        chs = (ch1, ch2)
+        labels = ("ch1", "ch2")
+        SetTimeseriesWindowing(folder+fname, time, chs, labels)
